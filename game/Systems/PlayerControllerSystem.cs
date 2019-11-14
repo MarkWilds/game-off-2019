233f846e8ed93554d1e58ae539270d3f7b837fed
Mark van der Wal <wilds.mark@gmail.com>
2019-11-14 15:35:37 +0100

    minor changes

--- game/Systems/PlayerControllerSystem.cs
+++ game/Systems/PlayerControllerSystem.cs
@@ -12,18 +12,16 @@ namespace game.Systems
     [With(typeof(Transform2D), typeof(Physics2D), typeof(Camera))]
     public class PlayerControllerSystem : AEntitySystem<double>
     {
-        private const float MouseSpeed = 20.0f;
-        private const double Deadzone = 2.0;
+        private const float MouseSpeed = 40.0f;
         private double bobTimer;
-        private int bobSpeed = 11;
-        private int bobOffset = 3;
-
-        private Vector2 accumelatedForce;
+        private int bobSpeed = 18;
+        private float bobOffset = 2f;
+        
         private const float dragFactor = 0.1f;
 
         public PlayerControllerSystem(World world) : base(world)
         {
-            accumelatedForce = Vector2.Zero;
+//            InputManager.CenterMouse = false;
         }
 
         protected override void Update(double deltaTime, in Entity entity)
@@ -34,10 +32,7 @@ namespace game.Systems
             ref var collider = ref entity.Get<IBox>();
             
             bobTimer += deltaTime * bobSpeed;
-            
-            var mouseDelta = InputManager.MouseAxisX;
-            if (Math.Abs(mouseDelta) > Deadzone)
-                transform.angle += mouseDelta * MouseSpeed * (float) deltaTime;
+            transform.angle += (float)(InputManager.MouseAxisX * MouseSpeed * deltaTime);
 
             double angleRad = transform.angle * Math.PI / 180;
             Vector2 forward = new Vector2((float) Math.Cos(angleRad),(float) Math.Sin(angleRad));
@@ -45,16 +40,16 @@ namespace game.Systems
 
             // force in the direction we want to move
             var accelerationDirection = forward * InputManager.VerticalAxis + right * InputManager.HorizontalAxis;
+            var accumulatedForce = Vector2.Zero;
             if (accelerationDirection.LengthSquared() > 0)
             {
                 accelerationDirection.Normalize();
-                accumelatedForce += accelerationDirection * physics2D.accelerationSpeed;
+                accumulatedForce += accelerationDirection * physics2D.accelerationSpeed;
             }
 
             // integration
-            physics2D.velocity += accumelatedForce * (float) deltaTime;
+            physics2D.velocity += accumulatedForce * (float) deltaTime;
             physics2D.velocity += physics2D.velocity * -dragFactor;
-            accumelatedForce = Vector2.Zero;
 
             // constraint
             var speed = physics2D.velocity.Length();
