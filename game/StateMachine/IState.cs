6b74da1482c497296922657e0b09367ca209be83
Mark van der Wal <wilds.mark@gmail.com>
2019-11-30 13:01:38 +0100

    Added statemachine stuff

---
+++ game/StateMachine/IState.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace game.StateMachine
+{
+    /// <summary>
+    /// Non-generic state interface.
+    /// </summary>
+    public interface IState
+    {
+        /// <summary>
+        /// Parent state, or null if this is the root level state.
+        /// </summary>
+        IState Parent { get; set; }
+
+        /// <summary>
+        /// Change to the state with the specified name.
+        /// </summary>
+        void ChangeState(string stateName);
+
+        /// <summary>
+        /// Push another state above the current one, so that popping it will return to the
+        /// current state.
+        /// </summary>
+        void PushState(string stateName);
+
+        /// <summary>
+        /// Exit out of the current state and enter whatever state is below it in the stack.
+        /// </summary>
+        void PopState();
+
+        /// <summary>
+        /// Update this state and its children with a specified delta time.
+        /// </summary>
+        void Update(float deltaTime);
+
+        /// <summary>
+        /// Triggered when we enter the state.
+        /// </summary>
+        void Enter();
+
+        /// <summary>
+        /// Triggered when we exit the state.
+        /// </summary>
+        void Exit();
+
+        /// <summary>
+        /// Trigger an event on this state or one of its children.
+        /// </summary>
+        /// <param name="name">Name of the event to trigger</param>
+        void TriggerEvent(string name);
+
+        /// <summary>
+        /// Triggered when and event occurs. Executes the event's action if the 
+        /// current state is at the top of the stack, otherwise triggers it on 
+        /// the next state down.
+        /// </summary>
+        /// <param name="name">Name of the event to trigger</param>
+        /// <param name="eventArgs">Arguments to send to the event</param>
+        void TriggerEvent(string name, EventArgs eventArgs);
+    }
+}
---
+++ game/StateMachine/IStateBuilder.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace game.StateMachine
+{
+    /// <summary>
+    /// Builder providing a fluent API for constructing states.
+    /// </summary>
+    public interface IStateBuilder<T, TParent> where T : AbstractState, new()
+    {
+        /// <summary>
+        /// Create a child state with a specified handler type. The state will take the
+        /// name of the handler type.
+        /// </summary>
+        /// <typeparam name="NewStateT">Handler type for the new state</typeparam>
+        /// <returns>A new state builder object for the new child state</returns>
+        IStateBuilder<NewStateT, IStateBuilder<T, TParent>> State<NewStateT>() where NewStateT : AbstractState, new();
+
+        /// <summary>
+        /// Create a named child state with a specified handler type.
+        /// </summary>
+        /// <typeparam name="NewStateT">Handler type for the new state</typeparam>
+        /// <param name="name">String for identifying state in parent</param>
+        /// <returns>A new state builder object for the new child state</returns>
+        IStateBuilder<NewStateT, IStateBuilder<T, TParent>> State<NewStateT>(string name) where NewStateT : AbstractState, new();
+
+        /// <summary>
+        /// Create a child state with the default handler type.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>A state builder object for the new child state</returns>
+        IStateBuilder<State, IStateBuilder<T, TParent>> State(string name);
+
+        /// <summary>
+        /// Set an action to be called when we enter the state.
+        /// </summary>
+        IStateBuilder<T, TParent> Enter(Action<T> onEnter);
+
+        /// <summary>
+        /// Set an action to be called when we exit the state.
+        /// </summary>
+        IStateBuilder<T, TParent> Exit(Action<T> onExit);
+
+        /// <summary>
+        /// Set an action to be called when we update the state.
+        /// </summary>
+        IStateBuilder<T, TParent> Update(Action<T, float> onUpdate);
+
+        /// <summary>
+        /// Set an action to be called on update when a condition is true.
+        /// </summary>
+        IStateBuilder<T, TParent> Condition(Func<bool> predicate, Action<T> action);
+
+        /// <summary>
+        /// Set an action to be triggerable when an event with the specified name is raised.
+        /// </summary>
+        IStateBuilder<T, TParent> Event(string identifier, Action<T> action);
+
+        /// <summary>
+        /// Set an action with arguments to be triggerable when an event with the specified name is raised.
+        /// </summary>
+        /// <typeparam name="TEvent"></typeparam>
+        /// <param name="identifier"></param>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        IStateBuilder<T, TParent> Event<TEvent>(string identifier, Action<T, TEvent> action) where TEvent : EventArgs;
+
+        /// <summary>
+        /// Finalise the current state and return the builder for its parent.
+        /// </summary>
+        TParent End();
+    }
+}
